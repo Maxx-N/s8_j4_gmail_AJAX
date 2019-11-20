f01197c29c973e77320f366c5c97edb4c7e9c6de
Maxx-N <maximenorval@gmail.com>
2019-11-20 15:28:53 +0100

    affichage de l'élément créé avec AJAX dans la bonne catégorie

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,7 @@ Category.destroy_all
 
 require 'faker'
 3.times do
-  my_category = Category.create(title: Faker::Book.genre)
+  my_category = Category.create(title: Faker::Book.unique.genre)
   3.times do
     my_task = Task.new(title: Faker::Book.title,
                       deadline: Faker::Date.forward(23),
