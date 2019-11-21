a0d17ac39fc727e1893af347ed294a00b0d62e66
Maxx-N <maximenorval@gmail.com>
2019-11-21 23:37:26 +0100

    positionnement des boutons

---
+++ app/assets/javascripts/object_square.js
@@ -0,0 +1,12 @@
+listOfSquares = document.getElementsByTagName("h6");
+
+for (let i = 0; i < listOfSquares.length; i++) {
+  let square = listOfSquares[i];
+  let onSquareClick = () => {
+    console.log(`Tu as cliqué sur le ${i+1}e objet`);
+  };
+  square.addEventListener("click", onSquareClick);
+};
+
+
+
